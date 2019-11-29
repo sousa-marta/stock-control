e0e2b8b356ad6af835d040edf6f5d16a5c48c4d3
Marta Sousa <marta.sousa@gmail.com>
2019-11-29 11:58:43 -0300

    tarefa de casa

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -42,4 +42,30 @@ class ProductController extends Controller
     public function viewForm(Request $request){   //poderia ser index para dizer que é a primeria função a ser executada
         return view('products.form');
     }
+
+    public function update(Request $request){
+        //Para atualizar, devemos buscar um objeto ao invés de criar:
+        //usando Product::find($idProduto)
+        //vai ser necessário usar rotas com parâmetro
+        // $newProduct = Product::find(3);
+        $newProduct->name = $request->nameProduct;
+        $newProduct->description = $request->descriptionProduct;
+        $newProduct->quantity = $request->quantityProduct;
+        $newProduct->price = $request->priceProduct;
+        $newProduct->user_id = Auth::user()->id; //auth é uma classe global que já existe no laravel que toda vez que usa o padrão de autenticação o laravel já salva o usuário nessa classe. user retorna informações do usuário logado no seu sistema
+
+        $result = $newProduct->save(); //retorna boleano
+    }
+
+    public function delete(Request $request){
+        // para deletar vc vai usar Product::destroy($id);
+    }
+
+    public function viewAllProducts(Request $request){
+        //vai precisar do Product::All
+    }
+
+    public function viewOneProduct(Request $request){
+        // vai precisar do Product::find($idProduct);
+    }
 }
